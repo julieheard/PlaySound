8d46f44d1be9cfa7ad93c71737ece8fd1223107a
Julie Heard <>
2021-02-03 15:00:06 +0000

    Initial Commit

---
+++ src/sound/MouseClicker.java
@@ -0,0 +1,38 @@
+
+package sound;
+import javax.swing.*;
+import java.net.*;
+import java.applet.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+        
+public class MouseClicker extends JFrame{
+    
+    AudioClip click;
+    
+    public static void main(String[] args) {
+        new MouseClicker();
+    }
+    
+    public MouseClicker(){
+        this.setSize(400,400);
+        this.setTitle("Mouse Clicker");
+        this.addMouseListener(new Clicker());
+        
+        URL urlClick = MouseClicker.class.getResource("sea.wav");
+        click=Applet.newAudioClip(urlClick);
+        
+        this.setVisible(true);
+        
+    }
+    private class Clicker extends MouseAdapter{
+        public void mouseClicked(MouseEvent e){
+            click.play();
+        }
+    }
+    
+
+}
+
+
